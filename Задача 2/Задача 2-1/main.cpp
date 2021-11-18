c35f7c66075d8ab25da51effd0d3857cb2dc364c
maksimlazukhin <91391045+maksimlazukhin@users.noreply.github.com>
2021-11-18 12:14:58 +0300

    Update and rename Task2/Task2-1/main.cpp to Задача 2/Задача 2-1/main.cpp

---
+++ Задача 2/Задача 2-1/main.cpp
@@ -0,0 +1,42 @@
+// Task2-1.
+#include <iostream>
+/**
+*\ brief  Объем паралепипеда
+*\ param ширина, длина, высота
+*\ return объем
+*/ 
+double getVolume(double widthP, double lengthP, double heightP);
+/**
+*\ brief  Площадь поверхности парралепипеда
+*\ param ширина, длина, высота
+*\ return  площадь
+*/
+double getArea(double widthP, double lengthP, double heightP);
+/**
+*\ brief  dвводим параметы парралепипеда
+*\ param выводим объем и площадб поверхности
+*\ return data 
+*/
+double vvod(std::string ss);
+int main()
+{
+    const double widthP = vvod("Enter the width ");
+    const double lengthP= vvod("Enter the length ");
+    const double heightP = vvod("Enter the  height ");
+    std::cout << "Volume=" << getVolume(widthP, lengthP, heightP) << " Area =" << getArea(widthP, lengthP, heightP);
+}
+double vvod(const std::string ss)
+{
+    std::cout<<ss;
+    double temp;
+    std::cin >> temp ;
+    return temp;
+}
+double getVolume(double widthP, double lengthP, double heightP)
+{
+    return  widthP * lengthP * heightP;
+}
+double getArea(double widthP, double lengthP, double heightP)
+{
+    return (widthP * lengthP + widthP * heightP + lengthP * heightP) * 2.0;
+}
