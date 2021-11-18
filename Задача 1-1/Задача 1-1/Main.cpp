a66ba94cea4dd2c6fdcb0e4b974f872424592659
maksimlazukhin <91391045+maksimlazukhin@users.noreply.github.com>
2021-11-18 12:00:39 +0300

    Update Main.cpp

--- Задача 1-1/Задача 1-1/Main.cpp
+++ Задача 1-1/Задача 1-1/Main.cpp
@@ -4,19 +4,19 @@
 #include <cmath>
 #include <iostream>
 /**
-* \ breaf  a(x,y,z)
-* \ param x,y,z
-* \ return A
+* \ brief  вычисление заданной функции a(x,y,z)
+* \ param переменные функции x,y,z
+* \ return вычмсленное значение функции A
 */
 double getA(const double x, const double y, const double z);
 /**
-* \ breaf  b(x,y,z)
-* \ param x,y,z
-* \ return B
+* \ brief вычисление заданной функции b(x,y,z)
+* \ param переменные функции x,y,z
+* \ return вычмсленное значение функции B
 */
 double getB(const double x, const double y, const double z);
 /**
-* \ breaf  main
+* \ brief функция  main
 * \ param 
 * \return 0 if success
 */
@@ -32,9 +32,9 @@ double getA(const double x, const double y, const double z)
 {
 	double ss;
 	ss = x * y * z + abs(z * sin(y));
-	if (ss < 0.)	// when extracting the root of the 3rd degree from the negative - an error
-	{				//in excel incorrectly calculates negative numbers. we take the root of a positive number
-		ss = -pow(-ss, 1. / 3.);	// then we change the sign
+	if (ss < 0.)	// wт.к получили отрицательное число, то функция вохзвращает ошибку
+	{				//поэтому если подкоренное выражение отрицательно0, то извлекаем корень из модуля и меням знак перед корнем
+		ss = -pow(-ss, 1. / 3.);	// корень 3 степени из отрицательного числа существует - он отрицательный
 	}
 	else
 	{
