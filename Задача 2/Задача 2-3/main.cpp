3bc0160a14bcd5ab40ad7fb8a6adbf6cb42ae97f
maksimlazukhin <91391045+maksimlazukhin@users.noreply.github.com>
2021-12-30 11:29:39 +0300

    Исправление замечаний

--- Задача 2/Задача 2-3/main.cpp
+++ Задача 2/Задача 2-3/main.cpp
@@ -6,46 +6,40 @@
 *\ param час
 *\ return приветствие
 */
-std::string vremy(int a);
+std::string mytime(int hour);
 /**
 *\ brief ввод часа
 *\ param строка запроса ввода
 *\ return час
 * */
-int input_int(std::string& ss);
+int input_int(std::string& message);
 int main()
 {
     std::string message = "enter the time  ";
-    double  a = input_int(message);
-    std::cout << vremy(a);
+    const auto hour = input_int(message);
+    std::cout << mytime(hour);
 }
-int input_int(std::string& ss)
+int input_int(std::string& message)
 {
-    std::cout << ss;
+    std::cout << message;
     int temp;
     std::cin >> temp;
     return temp;
 }
-std::string vremy(int a)
+std::string mytime(int hour)
 {
-    int const morning_start = 5;
-    int const afternoon_start = 12;
-    int const evening_start = 18;
-    int const night_start = 21;
-    if (a >= 12 && a <= 18)
-    {
-        return  "good afternoon";
+    /*
+    enum {
+        morning_start = 5,
+        afternoon_start = 12,
+        evening_start = 18,
+        night_start = 21
+    };*/
+    switch (hour) {
+    case 12: case 13: case 14: case 15: case 16: case 17: return  "good afternoon"; break;
+    case 5: case 6: case 7: case 8: case 9: case 10: case 11: return  "good morning"; break;
+    case 18: case 19: case 20: case 21: return "good evening"; break;
+    case 22: case 23: case 24: case 0: case 1: case 2: case 3: case 4: return "good night"; break;
     }
-    if (a < afternoon_start && a >= morning_start)
-    {
-        return  "good morning";
-    }
-    if (a >= evening_start && a <= night_start)
-    {
-        return "good evening";
-    }
-    if (a > night_start || a < morning_start)
-    {
-        return "good night";
-    }
+    return "Error!";
 }
