154b31b63a7793e7c763a8e38bc03b886068b088
maksimlazukhin <91391045+maksimlazukhin@users.noreply.github.com>
2021-12-30 12:29:45 +0300

    Исправление замечаний

--- Задача 3/Задача 3-1/main.cpp
+++ Задача 3/Задача 3-1/main.cpp
@@ -4,27 +4,36 @@
 /**
 *\ brief находит 
 *\ param x
-*\ return значение функции
+*\ return значение функции f(x) = 0.1x^2 - xln(x)
 */
-double funct(double x);
+double funct(const double x);
+/**
+*\ brief находит 
+*\ param x
+*\ return вычислима ли f(x) = 0.1x^2 - xln(x)
+*/
+bool checkValid(const double x);
 int main()
-{   
-    double x;
-    for (int counter =0; counter <=10; counter++)
+{
+    const double LEFT_X = 1.0;
+    const double RIGHT_X = 2.0;
+    const double DELTA = 0.1;
+    int max_counter = (int)((RIGHT_X - LEFT_X)/DELTA);
+    for (int counter = 0; counter <= max_counter; counter++)
     {
-        x = 1.0 + 0.1 * counter;
-        std::cout << "For x=" << x << " y= " << funct(x) << "\n";
+        double x = LEFT_X + DELTA * counter;
+        if (checkValid(x))
+            std::cout << "For x=" << x << " y= " << funct(x) << "\n";
+        else
+            std::cout << "Invalid x: " << x  << "\n";
     }
     return 0;
 }
-double funct(double x)
+double funct(const double x)
 {
-    if (x>0)
-    {
     return 0.1 * x * x - x * log(x);
-    }
-    else
-    {
-        std::cout << "Неправильный аргумент";
-    }
+}
+bool checkValid(const double x)
+{
+    return x > 0;
 }
