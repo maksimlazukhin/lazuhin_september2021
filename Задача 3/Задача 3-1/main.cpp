2da879cfc64daf0b6e6daee0a2c27adaacc3bd26
maksimlazukhin <91391045+maksimlazukhin@users.noreply.github.com>
2022-01-12 17:24:11 +0300

    Update main.cpp

--- Задача 3/Задача 3-1/main.cpp
+++ Задача 3/Задача 3-1/main.cpp
@@ -2,30 +2,35 @@
 #include <iostream>
 #include <cmath>
 /**
-*\ brief находит 
-*\ param x
-*\ return значение функции f(x) = 0.1x^2 - xln(x)
+*\ brief вычисляет значение заданной функции f(x) = 0.1x^2 - xln(x)
+*\ param x - аргумент
+*\ return значение функции
 */
 double funct(const double x);
 /**
-*\ brief находит 
+*\ brief проверяет корректность аргументф X для функции funct()
+*\ param x
+*\ return true если корректный аргумент, иначе false
+*/
+bool checkValid(const double x);
+/**
+*\ brief табудирует функцию funct() заданную на интерваое LEFT_X  - RIGHT_X с шагом  DELTA
 *\ param x
 *\ return вычислима ли f(x) = 0.1x^2 - xln(x)
 */
-bool checkValid(const double x);
 int main()
 {
     const double LEFT_X = 1.0;
-    const double RIGHT_X = 2.0;
+    const double RIGHT_X = 2.001;
     const double DELTA = 0.1;
-    int max_counter = (int)((RIGHT_X - LEFT_X)/DELTA);
-    for (int counter = 0; counter <= max_counter; counter++)
+    double x = LEFT_X;
+    while (x <= RIGHT_X)
     {
-        double x = LEFT_X + DELTA * counter;
         if (checkValid(x))
             std::cout << "For x=" << x << " y= " << funct(x) << "\n";
         else
-            std::cout << "Invalid x: " << x  << "\n";
+            std::cout << "Invalid x: " << x << "\n";
+        x += DELTA;
     }
     return 0;
 }
