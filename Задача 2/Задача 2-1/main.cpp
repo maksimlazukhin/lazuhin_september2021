e4fcb66c0360024dea466bdc991658a31416314e
maksimlazukhin <91391045+maksimlazukhin@users.noreply.github.com>
2022-01-21 13:22:15 +0300

    Update main.cpp

--- Задача 2/Задача 2-1/main.cpp
+++ Задача 2/Задача 2-1/main.cpp
@@ -22,7 +22,7 @@ double enter_double(const std::string& message);
 /// <summary>
 /// Что будем считать
 /// </summary>
-enum kind {
+enum class kind {
     VOLUME = 0,
     AREA = 1
 };
@@ -30,16 +30,16 @@ enum kind {
 
 int main()
 {
-    std::string message= "Enter the width ";
+    std::string message = "Enter the width ";
     const double widthP = enter_double(message);
     message = "Enter the length ";
     const double lengthP = enter_double(message);
     message = "Enter the  height ";
     const double heightP = enter_double(message);
     message = "Choose what you want to get (volume: 0, area: 1) ";
-    switch ((int)enter_double(message)) {
-        case VOLUME: std::cout << "Volume=" << getVolume(widthP, lengthP, heightP); break;
-        case AREA: std::cout << " Area =" << getArea(widthP, lengthP, heightP); break;
+    switch ((kind)enter_double(message)) {
+    case kind::VOLUME: std::cout << "Volume=" << getVolume(widthP, lengthP, heightP); break;
+    case kind::AREA: std::cout << " Area =" << getArea(widthP, lengthP, heightP); break;
     }
 }
 double enter_double(const std::string& message)
